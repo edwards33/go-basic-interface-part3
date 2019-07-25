8814004712e4d14b9700e3500d02b3a5800f295f
Edward <edylennyy@gmail.com>
2019-07-26 00:49:08 +0300

    initial

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://raw.githubusercontent.com/AllenDowney/OlinPyShop/master/wordsEn.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	bs, _ := ioutil.ReadAll(res.Body)
+	str := string(bs)
+	fmt.Println(str)
+
+}
